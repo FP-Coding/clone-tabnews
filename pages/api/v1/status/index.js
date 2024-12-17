75abfb45610d96e2f5fc2c26dd09b0cc8194ce2d
FP-Coding <felipe14062000@gmail.com>
2024-12-16 23:48:31 -0400

    chore: adição do .env e database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-const status = (request, response) => {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("SELECT 1+1 as SUM");
+  console.log(result.rows);
+
   return response.status(200).json({ Felipe: "Sou um aluno curso.dev" });
 };
 
