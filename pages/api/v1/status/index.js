babce5d1ce7beb5094df891105c6c34365fac872
Felipe Pinto <felipe14062000@gmail.com>
2024-12-21 14:53:29 +0000

    feat: adição de campos version, max_connections e opened_connections a página de status da api, implementação antes de assistir a aula

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,26 @@
 import database from "infra/database.js";
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT 1+1 as SUM");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const { rows: versionInfo } = await database.query("SELECT * FROM pg_settings WHERE name = 'server_version'");
+  const { rows: maxConnectionsInfo } = await database.query("SELECT * FROM pg_settings WHERE name ='max_connections'");
+  const { rows: openedConnectionsInfo } = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE datname='local_db'");
 
-  return response.status(200).json({ Felipe: "Sou um aluno curso.dev" });
+  const { setting: version } = versionInfo[0];
+  const { setting: maxConnections } = maxConnectionsInfo[0];
+  const { count: openedConnections } = openedConnectionsInfo[0];
+  
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        opened_connections: parseInt(openedConnections),
+      }
+    }
+  });
 };
 
 export default status;
