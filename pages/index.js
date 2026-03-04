039186902bacb044441cba5459f77685bdef7463
Felipe Pinto <felipe14062000@gmail.com>
2026-03-04 18:50:52 -0300

    feat: primeiro commit do clone tabnews

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div>
+            <h1>Você é o amor da minha, vida S2</h1>
+            <p>Estou com muita saudades, e mal espero para poder casar com você</p>
+        </div>
+    )
+    
+}
+
+export default Home;
