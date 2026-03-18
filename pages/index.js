d5578cc35e17dde541055fec46ea2129d089fac9
Felipe Pinto <felipe14062000@gmail.com>
2026-03-18 19:36:29 -0300

    chore: adiciona `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,8 @@ function Home() {
         <div>
             <h1>Você é o amor da minha, vida S2</h1>
             <p>Estou com muita saudades, e mal espero para poder casar com você</p>
-        </div>
+        </div>	
     )
-    
 }
 
 export default Home;
