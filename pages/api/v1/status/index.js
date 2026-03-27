61dd42cd14bc62e444537e1b9fe87732bbc0bb01
Felipe Pinto <felipe14062000@gmail.com>
2026-03-27 19:24:53 -0300

    refactor: muda nome da variável do api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -10,11 +10,11 @@ async function status(request, response) {
   const maxConnections = resultMaxConnections.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
-  const resultQuantityOfConectionsOpen = await database.query({
+  const resultOpenedConnections = await database.query({
     text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname=$1;",
     values: [databaseName],
   });
-  const openedConnections = resultQuantityOfConectionsOpen.rows[0].count;
+  const openedConnections = resultOpenedConnections.rows[0].count;
 
   response.status(200).json({
     update_at: updateAt,
